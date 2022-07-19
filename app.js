474beb09366c3804344d94374a7dc34ef7e12eb1
gurcancekic <gurcan.cekic@reengen.com>
2022-07-19 22:32:59 +0300

    lesson 02

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+//ejs template engine
+app.set('view engine', 'ejs');
+
+//static files middleware
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('INDEX SAYFASI 2');
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 app.listen(port, () => {
