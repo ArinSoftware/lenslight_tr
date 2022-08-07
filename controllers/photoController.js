fe8b9031b25cea18e7e0e7c0a4e5f31ad0d908be
gurcancekic <arinyazilim@gmail.com>
2022-08-07 23:14:06 +0300

    lesson 21

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -11,8 +11,6 @@ const createPhoto = async (req, res) => {
     }
   );
 
-  console.log('RESULT::', result);
-
   try {
     await Photo.create({
       name: req.body.name,
@@ -83,4 +81,40 @@ const deletePhoto = async (req, res) => {
   }
 };
 
-export { createPhoto, getAllPhotos, getAPhoto, deletePhoto };
+const updatePhoto = async (req, res) => {
+  try {
+    const photo = await Photo.findById(req.params.id);
+
+    if (req.files) {
+      const photoId = photo.image_id;
+      await cloudinary.uploader.destroy(photoId);
+
+      const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+          use_filename: true,
+          folder: 'lenslight_tr',
+        }
+      );
+
+      photo.url = result.secure_url;
+      photo.image_id = result.public_id;
+
+      fs.unlinkSync(req.files.image.tempFilePath);
+    }
+
+    photo.name = req.body.name;
+    photo.description = req.body.description;
+
+    photo.save();
+
+    res.status(200).redirect(`/photos/${req.params.id}`);
+  } catch (error) {
+    res.status(500).json({
+      succeded: false,
+      error,
+    });
+  }
+};
+
+export { createPhoto, getAllPhotos, getAPhoto, deletePhoto, updatePhoto };
--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -10,5 +10,6 @@ router
 
 router.route('/:id').get(photoController.getAPhoto);
 router.route('/:id').delete(photoController.deletePhoto);
+router.route('/:id').put(photoController.updatePhoto);
 
 export default router;
